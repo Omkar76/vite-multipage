d692148cca99314576552979b89f279871435b24
Onkar Jadhav <omjadhav2610gmail.com>
2023-01-21 15:41:47 +0530

    Initial commit

---
+++ src/reports/report.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <div>This is reports page</div>
+  </React.StrictMode>
+)
